e079386759ea2fba335f11232f4c3275f3e7beb5
Felix Tuffreaud <felix.tuffreaud@laposte.net>
2021-05-19 23:12:18 +0200

    modif template, add education

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\ProfessionnalExperience;
+use App\Repository\EducationRepository;
+use App\Repository\HobbyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,15 +18,31 @@ class PortfolioController extends AbstractController
     }
 
     #[Route('/experience', name: 'experience')]
-    public function experience(): Response
+    public function getExperiences(): Response
     {
-        return $this->render('portfolio/experience.html.twig');
+        $repo = $this->getDoctrine()->getRepository(ProfessionnalExperience::class);
+        $experiences = $repo->findBy([], [ 'year' => 'DESC']);
+        return $this->render('portfolio/experience.html.twig', [
+            'experiences' => $experiences
+        ]);
     }
 
     #[Route('/hobby', name: 'hobby')]
-    public function hobby(): Response
+    public function gethobbies(HobbyRepository $hobbyRepository): Response
     {
-        return $this->render('portfolio/hobby.html.twig');
+        $hobbies = $hobbyRepository->findAll();
+        return $this->render('portfolio/hobby.html.twig', [
+            'hobbies' => $hobbies
+        ]);
+    }
+
+    #[Route('/education', name: 'education')]
+    public function getEducations(EducationRepository $educationRepository): Response
+    {
+        $educations = $educationRepository->findAll();
+        return $this->render('portfolio/education.html.twig', [
+            'educations' => $educations
+        ]);
     }
 
     #[Route('/contact', name: 'contact')]
